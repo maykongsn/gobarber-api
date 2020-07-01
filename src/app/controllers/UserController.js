aea21ff7e114558e70a9b7311627c42cf4b61070
maykongsn <contatomaykon@hotmail.com>
2020-07-01 10:19:26 -0300

    add user data update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,6 +19,35 @@ class UserController {
       provider,
     });
   }
+
+  async update(request, response) {
+    const { email, oldPassword } = request.body;
+
+    const user = await User.findByPk(request.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return response.status(401).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return response.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name, provider } = await user.update(request.body);
+
+    return response.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
