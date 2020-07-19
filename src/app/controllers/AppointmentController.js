03dae42cea4fb53b6af380a519f6969205a8d979
maykongsn <contatomaykon@hotmail.com>
2020-07-19 09:26:33 -0300

    add appointments validation

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -27,11 +28,38 @@ class AppointmentController {
         .status(401)
         .json({ error: 'You can only appointments with providers' });
     }
+    /**
+     * Check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return response
+        .status(400)
+        .json({ error: 'Past date are not permitted' });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return response
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
 
     const appointment = await Appointment.create({
       user_id: request.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return response.json(appointment);
