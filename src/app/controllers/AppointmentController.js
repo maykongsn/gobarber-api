5ec149d62359044a1ca52b3ed9e03715034663e6
maykongsn <contatomaykon@hotmail.com>
2020-07-19 10:26:13 -0300

    add appointments pagination

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,9 +6,13 @@ import Appointment from '../models/Appointment';
 
 class AppointmentController {
   async index(request, response) {
+    const { page = 1 } = request.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: request.userId, canceled_at: null },
       order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       attributes: ['id', 'date'],
       include: [
         {
